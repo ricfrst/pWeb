247a426728f1a9f6c311a54c58254d5657bbab7a
Rick <ricardofurst06@gmail.com>
2025-04-23 18:48:39 -0400

    alignment fix

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -3,16 +3,17 @@ import Image from "next/image";
 export default function Sidebar(){
     return(
         <div className=" MAIN SIDE flex flex-col w-[33%] items-end mt-15">
-            <div className="PFP flex flex-col m-4 gap-3 items-end ">
+            <div className="PFP flex flex-col m-4 gap-2 items-end ">
                 <Image src={"/please.jpg"}
                        alt='pfp'
                        width="60"
                        height="60"
                        className="rounded-full"/>
                 <a className="font-bold">Rick Furst</a>
+                <a className="font-">Student @ Florida State</a>
 
             </div>
-            <div role="tablist" className="tabs flex-col mt-3 tabs-border w-[45%] items-end">
+            <div role="tablist" className="tabs flex-col mt- tabs-border w-[45%] items-end">
                 <a role="tab" className="tab">About</a>
                 <a role="tab" className="tab">Portfolio</a>
                 <a role="tab" className="tab tab-active">Reading List</a>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default async function Page() {
 
     return (
 
-        <div className="MAIN flex border  flexbox h-full w-screen ">
-            <div className = "m-4 flex justify-between">
+        <div className="MAIN flex border  flexbox h-full ">
+            <div className = "m-4 flex justify-between mr-33">
                 <h1>  Hey there! Im Rick,  an aspiring software engineer, and am currently studying CS and finance at Florida State University!  </h1>
             </div>
 
